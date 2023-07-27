e7c70837393860e9fe4d5ebe03867c2b8cdb9bca
Vladyslav Aleksyn <120416059+AleksynVladyslav@users.noreply.github.com>
2023-07-27 20:37:39 +0200

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import shortid from 'shortid';
+// import css from './App.module.css';
+
+class App extends Component {
+  state = {};
+
+  componentDidMount(nextProps, prevState) {}
+
+  componentDidUpdate(prevProps, prevState, snapshot) {}
+
+  render() {
+    return;
+  }
+}
+
+export default App;
